c0dd79245f5c3d100bd6c4510ffb9641c77f62e8
Kaian Santos <kaianfsantos@gmail.com>
2024-01-07 22:08:07 -0300

    chore: removing leftover code from the proejct

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <div v-for="brand in brands" :key="brand.id">
+      {{ brand.name }}
+    </div>
   </main>
 </template>
+<script>
+  import { getAll as getAllBrands } from '../api/brand'
+  export default {
+    data: () => ({
+      brands: []
+    }),
+    async mounted () {
+      this.brands = await getAllBrands()
+    }
+  }
+</script>
