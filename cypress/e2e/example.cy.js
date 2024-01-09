4ef8d7059e93c61d93caac93da452ed6a36938b4
Kaian Santos <kaianfsantos@gmail.com>
2024-01-09 20:25:24 -0300

    test: changing subjects usd in tests with cypress

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -1,8 +1,12 @@
 // https://on.cypress.io/api
 
-describe('My First Test', () => {
-  it('visits the app root url', () => {
+describe('integration test', () => {
+  it('DOM Was loaded succesfully', () => {
     cy.visit('/')
-    cy.contains('h1', 'You did it!')
+    cy.contains('div', 'Brands')
+    cy.contains('div', 'Cars')
+    cy.contains('div', 'Create a new Brand')
+    cy.contains('div', 'Create a new Car')
+    cy.contains('div', 'ford')
   })
 })
