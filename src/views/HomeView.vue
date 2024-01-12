f5e5be6f7ccf158055fb35c89a2f0618550554d3
Kaian Santos <kaianfsantos@gmail.com>
2024-01-11 21:00:52 -0300

    feat: creating homepage view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,46 +1,15 @@
 <template>
-  <main>
-    <div class="content">
-      <div class="card" id="brands">
-        <div class="title">
-          Brands
-        </div>
-        <div
-          class="list" 
-          v-for="brand in brands" 
-          :key="brand._id"
-          :id="'brand-'+ brand.name"
-          >
-          {{ brand.name }}
-        </div>
-      </div>
-      <div class="card" id="cars">
-        <div class="title">
-          Cars
-        </div>
-        <div
-          class="list" 
-          v-for="car in cars" 
-          :key="car._id"
-          :id="'car-'+ car.model"
-        >
-          {{ car.model }}
-        </div>
-      </div>
-      <NewBrandComponent :reloadData="loadData"/>
-      <NewCarComponent :brands="brands" :reloadData="loadData"/>
-    </div>
-  </main>
+  <div class="content">
+    <FilterComponent :brands="brands"/>
+  </div>
 </template>
 <script>
   import { getAll as getAllBrands } from '../api/brand'
   import { getAll as getAllCars } from '../api/car'
-  import NewCarComponent from '@/components/NewCarComponent.vue'
-  import NewBrandComponent from '@/components/NewBrandComponent.vue'
+  import FilterComponent from '@/components/FilterComponent.vue'
   export default {
     components: {
-      NewCarComponent,
-      NewBrandComponent
+      FilterComponent
     },
     data: () => ({
       brands: [],
@@ -63,9 +32,6 @@
   }
 </script>
 <style scoped>
-main {
-  padding: 32px;
-}
 .content {
   display: flex;
   gap: 16px;
